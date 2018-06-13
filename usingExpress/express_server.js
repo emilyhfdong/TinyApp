3b9a47db8f49c3d61df87d3482f3b4c6aefa220a
Emily Dong <emilyhfdong@gmail.com>
2018-06-13 15:35:14 +0000

    added error page and link to go back to url index

--- usingExpress/express_server.js
+++ usingExpress/express_server.js
@@ -7,6 +7,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
 function generateRandomString() {
   const lettersAndNums = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'.split('');
   var str = '';
@@ -22,56 +26,72 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//GET home page
 app.get("/", (req, res) => {
   res.end("Hello!");
 });
 
+//GET json
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//GET hello page
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//GET url index page
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+//GET new url form page
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+//POST new url (from new url page)
 app.post("/urls", (req, res) => {
   let newShort = generateRandomString();
+  while (urlDatabase[newShort]) {
+    newShort = generateRandomString;
+  }
   urlDatabase[newShort] = req.body.longURL;
 
   res.redirect("http://localhost:8080/urls/" + newShort);
 });
 
-app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id]};
-  res.render("urls_show", templateVars);
+//GET show/edit existing short URL page
+app.get("/urls/:shortURL", (req, res) => {
+  if (!urlDatabase[req.params.shortURL]) {
+    res.status(404).render("404");
+  } else {
+    let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+    res.render("urls_show", templateVars);
+  }
 });
 
+//GET redirected long URL from shortURL
 app.get("/u/:shortURL", (req, res) => {
   let longURL = urlDatabase[req.params.shortURL];
   if (!urlDatabase[req.params.shortURL]) {
-    res.status(400);
-    res.send('shortened URL does not exist');
+    res.status(404).render("404");
   } else {
     res.status(301);
     res.redirect(longURL);
   }
 });
 
+//DELETE url (from url index page)
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect("http://localhost:8080/urls/");
 
 });
 
+//UPDATE url (from show URL page)
 app.post("/urls/:id/", (req, res) => {
   urlDatabase[req.params.id] = req.body.longURL;
   res.redirect("http://localhost:8080/urls/");
@@ -82,6 +102,3 @@ app.post("/urls/:id/", (req, res) => {
 
 
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
